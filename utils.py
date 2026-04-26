8e557e0a68887cd4f1122a70cf86abc1cd036f01
bczuniga <61818710+bczuniga@users.noreply.github.com>
2026-04-25 19:51:23 -0700

    Added training loop, account for new sensors
    Loading data into DataLoaders for train, test, and validation (80-10-10). Added metrics logic for loss visualization.

--- utils.py
+++ utils.py
@@ -1,23 +1,16 @@
 import torch
 import pandas as pd
-from PIL import Image
 from torch.utils.data import Dataset, DataLoader, RandomSampler, BatchSampler
 from torchvision import transforms
 
+
 class DrivingDataset(Dataset):
     """
-    X : frame
+    X = distFromCenter, headingAngle, xCurvature, yCurvature, roadWidth, drivability
     y = steering, throttle, brake
     """
     def __init__(self, df: pd.DataFrame):
         self.df = df
-        self.frames_dir = df["Frame"]
-
-        # shrinks image down to 128x128 from 512x512
-        self.transform = transforms.Compose([
-            transforms.Resize((128, 128)),
-            transforms.ToTensor(),
-        ])
 
     def __len__(self):
         return len(self.df)
@@ -25,13 +18,19 @@ class DrivingDataset(Dataset):
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
 
-        X = Image.open(row["Frame"]).convert("RGB")
-        X = self.transform(X)
+        X = torch.tensor([
+            row["distFromCenter"],
+            row["headingAngle"],
+            row["xCurvature"],
+            row["yCurvature"],
+            row["roadWidth"],
+            row["drivability"]
+        ])
 
         y = torch.tensor([
             row["Steering"],
             row["Throttle"],
             row["Brake"],
         ], dtype=torch.float32)
+
         return X, y
-        
